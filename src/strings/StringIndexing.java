e4a2d31636d6e34244f38ec59ca9308916cc251a
Baktybek Tazhibaev <baktybektazhibaev@gmail.com>
2021-11-09 21:18:02 -0600

    String intro, concat, length, charAt

---
+++ src/strings/StringIndexing.java
@@ -0,0 +1,22 @@
+package strings;
+
+public class StringIndexing {
+
+    // indexing is basically a position that gets stored at certain index in String objects
+    // Devx:
+    //      'D' -> index [0]
+    //      'e' -> index [1]
+    //      'v' -> index [2]
+    //      'x' -> index [3]
+
+    // every special character can be a char in a String
+    // and white spaces in String variables are also indexed because
+    // every char "" that's in between double quotes is a String char
+
+    public static void main(String[] args) {
+
+        String school = "Devx School"; // white space between Devx and School is also indexed
+
+
+    }
+}
